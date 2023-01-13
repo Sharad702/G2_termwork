a79ab703c0123e15d97a3bef46937be4855151e9
Sharad702 <89999342+Sharad702@users.noreply.github.com>
2023-01-13 21:18:49 +0530

    Create Fractional_knapsack.cpp

---
+++ Fractional_knapsack.cpp
@@ -0,0 +1,52 @@
+//   FRACTIONAL KNAPSACK
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool comp(pair<int,int>a, pair<int,int>b)
+{
+   double r1 = (double)a.first/a.second;
+   double r2 = (double)b.first/b.second;
+
+   return r1>r2;   
+}
+
+void fractional_knapsack(pair<int,int>array[], int size, int weight)
+{
+    sort(array, array+size, comp);
+    double res = 0;
+    for(int i  = 0; i < size; i++)
+    {
+        if(array[i].second <= weight)
+        {
+           res = res + array[i].first;
+           weight = weight - array[i].second;
+        }
+        else
+        {
+            res = res + array[i].first*((double)weight/array[i].second);
+            break;
+        }
+    }
+    cout<<"Maximum value can be obtained : "<<res;
+}
+
+
+int main()
+{
+    int size;
+    cout<<"Enter the number of total items : ";
+    cin>>size;
+    pair<int,int> array[size];
+
+    cout<<"Enter the weight and value of items : ";
+    for(int i = 0; i < size; i++)
+    {
+        cin>>array[i].first>>array[i].second;
+    }
+    int weight;
+    cout<<"Enter the weight of knapsack : ";
+    cin>>weight;
+    fractional_knapsack(array, size, weight);
+}
